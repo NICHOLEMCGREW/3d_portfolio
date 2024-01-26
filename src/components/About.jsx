888d166590a7a67a875d652f6eeba0cb2987930f
Nichole McGrew <nicholemcgrew@gmailcom>
2024-01-26 08:13:28 -0600

    Resolved merge conflicts in About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,11 +19,7 @@ const ServiceCard = ({ index, title, icon }) => (
           scale: 1,
           speed: 450,
         }}
-<<<<<<< HEAD
         className='bg-[#915EFF] rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-=======
-        className='bg-[#a582ef] rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
->>>>>>> 102c4978ece880848e3e326fd586aec50c27ad30
       >
         <img
           src={icon}
