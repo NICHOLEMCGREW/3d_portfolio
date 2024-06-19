2461fa04435d73149efb98e9e9d7fe58f6c5373b
Nichole McGrew <nicholemcgrew@gmail.com>
2024-06-19 18:54:45 -0500

    updated project text

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -91,7 +91,7 @@ const Projects = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} `}>My work</p>
+        {/* <p className={`${styles.sectionSubText} `}>My work</p> */}
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
 
@@ -100,11 +100,7 @@ const Projects = () => {
           variants={fadeIn('', '', 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-          The following projects showcase my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          Check out my most recent projects.
         </motion.p>
       </div>
 
