0ee16001e56c27edb65bd1a0d9759fe2ce1c8eb3
Nichole McGrew <nicholemcgrew@gmail.com>
2024-06-19 18:57:28 -0500

    commented out p tags

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -92,19 +92,24 @@ const Projects = () => {
     <>
       <motion.div variants={textVariant()}>
         {/* <p className={`${styles.sectionSubText} `}>My work</p> */}
-        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+        <h2 className={`${styles.sectionHeadText}`}>My Work.</h2>
       </motion.div>
 
       <div className="w-full flex">
         <motion.p
           variants={fadeIn('', '', 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+          className="mt- text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           Check out my most recent projects.
+          {/* The following projects showcase my skills and experience through
+          real-world examples of my work. Each project is briefly described with
+          links to code repositories and live demos in it. It reflects my
+          ability to solve complex problems, work with different technologies,
+          and manage projects effectively. */}
         </motion.p>
       </div>
 
-      <div className="mt-20 flex flex-wrap gap-7">
+      <div className="mt-10 flex flex-wrap gap-7">
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
