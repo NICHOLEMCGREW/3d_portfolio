fa6a2a00ad8c59be1b3d7645e3bf6de512964e7f
Nichole McGrew <nicholemcgrew@gmail.com>
2024-06-19 18:27:25 -0500

    updated project card flex

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,6 @@
 import PropTypes from 'prop-types';
-import { Tilt } from 'react-tilt';
 import { motion } from 'framer-motion';
-import { styles } from '../styles';
 import { github, netlify } from '../assets';
-import { SectionWrapper } from '../hoc';
-import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 
 const ProjectCard = ({
@@ -33,14 +29,7 @@ const ProjectCard = ({
   return (
     <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)}>
       <div className="project-card" onClick={handleCardClick}>
-        <div
-          // options={{
-          //   max: 1,
-          //   scale: 0,
-          //   speed: 0,
-          // }}
-          className="relative p-5 rounded-2xl h-[530px] sm:w-[360px] w-full border-2 border-gradient cursor-pointer"
-        >
+        <div className="relative p-5 rounded-2xl border-2 border-gradient cursor-pointer">
           <div className="w-full h-[230px]">
             <img
               src={image}
@@ -92,36 +81,6 @@ const ProjectCard = ({
   );
 };
 
-const Projects = () => {
-  return (
-    <>
-      <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} `}>My work</p>
-        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-      </motion.div>
-
-      <div className="w-full flex">
-        <motion.p
-          variants={fadeIn('', '', 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
-        >
-          The following projects showcase my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
-        </motion.p>
-      </div>
-
-      <div className="mt-20 flex flex-wrap gap-7">
-        {projects.map((project, index) => (
-          <ProjectCard key={`project-${index}`} index={index} {...project} />
-        ))}
-      </div>
-    </>
-  );
-};
-
 // Prop type validation
 ProjectCard.propTypes = {
   index: PropTypes.number.isRequired,
@@ -140,4 +99,4 @@ ProjectCard.propTypes = {
   }).isRequired,
 };
 
-export default SectionWrapper(Projects, 'projects');
+export default ProjectCard;
